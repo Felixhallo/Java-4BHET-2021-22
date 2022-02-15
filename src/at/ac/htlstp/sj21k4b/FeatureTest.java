9db27bdaff10d69817fa753dd306be399dea59c0
Felix Schwab <felix.schwab@htlstp.at>
2022-02-15 08:56:12 +0100

    Hallo Welt

---
+++ src/at/ac/htlstp/sj21k4b/FeatureTest.java
@@ -0,0 +1,9 @@
+package at.ac.htlstp.sj21k4b;
+
+public class FeatureTest {
+
+    public static void main(String[] args) {
+        Sytem.out.println("Feature Test.");
+    }
+
+}
---
+++ src/at/ac/htlstp/sj21k4b/WiderstandKondensatorBerechnung.java
@@ -0,0 +1,12 @@
+package at.ac.htlstp.sj21k4b;
+
+import java.util.Scanner;
+
+public class WiderstandKondensatorBerechnung {
+
+    public static void main(String[] args) {
+
+        System.out.println();
+
+    }
+}
