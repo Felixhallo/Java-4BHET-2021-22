1be67801e436594f8b1f514dcce035770ed9eee8
Felix Schwab <felix.schwab@htlstp.at>
2022-02-15 08:57:08 +0100

    Hallo Welt

--- src/at/ac/htlstp/sj21k4b/FeatureTest.java
+++ src/at/ac/htlstp/sj21k4b/FeatureTest.java
@@ -3,7 +3,7 @@ package at.ac.htlstp.sj21k4b;
 public class FeatureTest {
 
     public static void main(String[] args) {
-        Sytem.out.println("Feature Test.");
+        System.out.println("Feature Test.");
     }
 
 }
