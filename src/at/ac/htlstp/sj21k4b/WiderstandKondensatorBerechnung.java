2b2d9121cda203d2c3c8695b8114a54b1653d678
Felix Schwab <felix.schwab@htlstp.at>
2022-02-15 09:14:39 +0100

    Hallo Welt

--- src/at/ac/htlstp/sj21k4b/WiderstandKondensatorBerechnung.java
+++ src/at/ac/htlstp/sj21k4b/WiderstandKondensatorBerechnung.java
@@ -4,9 +4,22 @@ import java.util.Scanner;
 
 public class WiderstandKondensatorBerechnung {
 
+    public static void Widerstandberechnung(int x,y) {
+
+    }
+
     public static void main(String[] args) {
 
-        System.out.println();
+        // Variablen
+        double w,k;
+
+        System.out.println("Für Widerstandsberechnung 1, für Kondensatorberechnung 2:");
+
+        if (sc.nextDouble()==1) {
+            Widerstandberechnung();
+        }
+
+        r1 = sc.nextDouble();
 
     }
 }
