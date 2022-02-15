75a972c03725840a110ebfc05192864864b196d8
Felix Schwab <felix.schwab@htlstp.at>
2022-02-15 09:20:02 +0100

    Master Test

--- src/at/ac/htlstp/sj21k4b/WiderstandKondensatorBerechnung.java
+++ src/at/ac/htlstp/sj21k4b/WiderstandKondensatorBerechnung.java
@@ -4,22 +4,30 @@ import java.util.Scanner;
 
 public class WiderstandKondensatorBerechnung {
 
-    public static void Widerstandberechnung(int x,y) {
-
-    }
-
     public static void main(String[] args) {
-
-        // Variablen
-        double w,k;
 
         System.out.println("Für Widerstandsberechnung 1, für Kondensatorberechnung 2:");
 
         if (sc.nextDouble()==1) {
-            Widerstandberechnung();
+
+            // Variablen deklarieren
+            double r1,r2,rp,rs;
+            Scanner sc = new Scanner(System.in);
+
+            // Werte einlesen
+            System.out.println("R1=");
+            r1 = sc.nextDouble();
+            System.out.println("R2=");
+            r2 = sc.nextDouble();
+
+            // Berechnung
+            rp = (r1*r2)/(r1+r2);
+            rs = (r1+r2);
+
+            System.out.printf("Parallel: %.2f Serie: %f\n", rp, rs);
         }
 
-        r1 = sc.nextDouble();
+        if (else)
 
     }
 }
